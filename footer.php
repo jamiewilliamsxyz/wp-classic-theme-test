195bdcb65f0117885db119a937b4ec0537229d42
Jamie <jamie7williams@gmail.com>
2025-12-28 15:53:21 +0000

    Add target/rel attributes to anchor tag

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer class="footer text-center py-2 theme-bg-dark">
-  <p class="copyright"><a href="https://github.com/jamiewilliamsxyz">jamiewilliamsxyz (GitHub)</a></p>
+  <p class="copyright"><a href="https://github.com/jamiewilliamsxyz" rel="noopener" target="_blank">jamiewilliamsxyz (GitHub)</a></p>
 </footer>
 
 </div>
