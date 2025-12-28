f2885784dcad9322cdd7d0c5dafe682199b1e920
Jamie <jamie7williams@gmail.com>
2025-12-28 15:33:30 +0000

    Seperate header/footer

--- front-page.php
+++ front-page.php
@@ -1,18 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php get_header() ?>
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>jamie-classic-theme</title>
-
-  <?php wp_head() ?>
-</head>
-
-<body>
-
-  <?php wp_footer() ?>
-
-</body>
-
-</html>
+<?php get_footer() ?>
