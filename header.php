ddbfaf5d42313329bc7dddc007275472e32d0766
Jamie <jamie7williams@gmail.com>
2025-12-28 15:57:51 +0000

    Show posts; make heading dynamic

--- header.php
+++ header.php
@@ -51,7 +51,9 @@
   </header>
 
   <header class="page-title theme-bg-light text-center gradient py-5">
-    <h1 class="heading">Blog Home Page Heading</h1>
+    <h1 class="heading">
+      <?php the_title() ?>
+    </h1>
   </header>
 
   <div class="main-wrapper">
