10f6a36414d4d0fe46df2c23f5515365e562e3d4
Jamie <jamie7williams@gmail.com>
2025-12-28 16:21:51 +0000

    Update meta tags

--- header.php
+++ header.php
@@ -2,8 +2,12 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Jamie Classic Theme Test">
+  <link rel="shortcut icon" href="./assets/images/logo.png">
+
   <?php wp_head() ?>
 </head>
 
