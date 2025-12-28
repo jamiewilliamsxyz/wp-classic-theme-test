d8b388a517f9734c3f7fa8be8417c30a54d72f76
Jamie <jamie7williams@gmail.com>
2025-12-28 14:50:24 +0000

    Add dynamic version to stylesheet

--- functions.php
+++ functions.php
@@ -4,7 +4,9 @@ add_action("wp_enqueue_scripts", "jct_register_styles");
 
 function jct_register_styles()
 {
-  wp_enqueue_style("jct-style", get_template_directory_uri() . "/style.css", array("jct-bootstrap"));
+  $version = wp_get_theme()->get("Version");
+
+  wp_enqueue_style("jct-style", get_template_directory_uri() . "/style.css", array("jct-bootstrap"), $version);
   wp_enqueue_style("jct-bootstrap", "https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css", [], "4.4.1");
   wp_enqueue_style("jct-fontawesome", "https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.13.0/css/fontawesome.min.css", [], "5.13.0");
 }
