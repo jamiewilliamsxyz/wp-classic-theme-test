7a9df9be72f2d5746623df3ab6fd9ba169147968
Jamie <jamie7williams@gmail.com>
2025-12-29 17:47:05 +0000

    Add search form

--- 404.php
+++ 404.php
@@ -3,6 +3,7 @@
 <article class="content px-3 py-5 p-md-5">
   <h2>404</h2>
   <h3>Page not found</h3>
+  <?php get_search_form() ?>
   <a href="<?php echo site_url() ?>">Return home</a>
 </article>
 
