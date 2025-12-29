263f9daffbf273693c2defc717e55c15d7fbfb86
Jamie <jamie7williams@gmail.com>
2025-12-29 17:25:53 +0000

    Create 404 page

---
+++ 404.php
@@ -0,0 +1,9 @@
+<?php get_header() ?>
+
+<article class="content px-3 py-5 p-md-5">
+  <h2>404</h2>
+  <h3>Page not found</h3>
+  <a href="<?php echo site_url() ?>">Return home</a>
+</article>
+
+<?php get_footer() ?>
